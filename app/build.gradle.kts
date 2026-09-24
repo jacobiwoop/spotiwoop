6ebbedc395778ccc311ddcb7f166b5147121842e
jacobiwoop <jacobiwoop@gmail.com>
2026-09-24 22:35:59 +0200

    chore: bump version to 0.1.5 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.spotywoop.kt"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
     }
 
     splits {
